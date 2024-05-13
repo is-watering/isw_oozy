b108523f71e1e4718fcff6fa6f3ed998ee53cee2
is-watering <24697900+is-watering@users.noreply.github.com>
2024-05-14 03:31:36 +0900

    update

--- firmware/qmk/src/keymaps/via_az1uball/keymap.c
+++ firmware/qmk/src/keymaps/via_az1uball/keymap.c
@@ -4,11 +4,11 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-		KC_F1,   KC_F2,   KC_F3,  KC_F4,    KC_F5,    KC_F6,           KC_F7,         KC_F8,                 KC_F9,                 KC_F10,  KC_F11,        KC_F12,
-		KC_TAB,  KC_Q,    KC_W,   KC_E,     KC_R,     KC_T,            KC_Y,          KC_U,                  KC_I,                  KC_O,    KC_P,          KC_BSPC,
-		KC_LSFT, KC_A,    KC_S,   KC_D,     KC_F,     KC_G,            KC_H,          KC_J,                  KC_K,                  KC_L,    LT(2,KC_SCLN), MT(MOD_RSFT, KC_QUOT),
-		KC_LCTL, KC_Z,    KC_X,   KC_C,     KC_V,     KC_B,            KC_N,          KC_M,                  MT(MOD_RGUI, KC_COMM), KC_DOT,  KC_UP,         KC_BTN3,
-		MO(1),   KC_LGUI,         KC_LALT,  KC_LCTL,  LT(1,KC_SPC),    LT(2,KC_ENT),  MT(MOD_RCTL, KC_SLSH), KC_RALT,               KC_LEFT, KC_DOWN,       KC_RGHT
+		KC_F1,   KC_F2,      KC_F3,              KC_F4,   KC_F5,                KC_F6,           KC_F7,        KC_F8,                 KC_F9,                 KC_F10,     KC_F11,                KC_F12,
+		KC_TAB,  LT(1,KC_Q), KC_W,               KC_E,    KC_R,                 KC_T,            KC_Y,         KC_U,                  KC_I,                  KC_O,       KC_P,                  KC_BSPC,
+		KC_LSFT, KC_A,       KC_S,               KC_D,    KC_F,                 KC_G,            KC_H,         KC_J,                  KC_K,                  LT(2,KC_L), MT(MOD_RSFT, KC_SCLN), MT(MOD_RCTL, KC_QUOT),
+		KC_LCTL, KC_Z,       MT(MOD_LSFT, KC_X), KC_C,    KC_V,                 KC_B,            KC_N,         KC_M,                  MT(MOD_RGUI, KC_COMM), KC_DOT,     KC_UP,                 KC_BTN3,
+		MO(1),   KC_LGUI,                        KC_LALT, MT(MOD_LCTL, KC_TAB), LT(1,KC_SPC),    LT(2,KC_ENT), MT(MOD_RALT, KC_SLSH), KC_BSPC,               KC_LEFT,    KC_DOWN,               KC_RGHT
 	),
 
 	[1] = LAYOUT(
